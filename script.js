826e26e94018ee9585f1aa57418d2c28222c14ad
maggie-wilson <magpie1025@icloud.com>
2026-08-29 20:26:09 +0000

    Making things prettier

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let button = document.querySelector(".button");
 
-function onButtonClick() {
-    button.style.color = "blue";
-}
 
-button.addEventListener("click", onButtonClick)
+
+button.addEventListener("mouseenter", onButtonClick)
